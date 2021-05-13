7fe03f0c159df06e811fe1280acbec612637de8f
Misha-13 <m.a.cherkasov@yandex.ru>
2021-05-14 00:26:40 +0300

    Добавляет шрифты и графику

--- gulpfile.js
+++ gulpfile.js
@@ -38,8 +38,9 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/img/*.{png,jpg,svg}", gulp.series("copyimg", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
@@ -93,9 +94,15 @@ gulp.task("copy", function () {
   .pipe(gulp.dest("build"));
 });
 
+gulp.task("copyimg", function () {
+  return gulp.src("source/img/**")
+  .pipe(gulp.dest("build/img"));
+});
+
+
 gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "webp", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
